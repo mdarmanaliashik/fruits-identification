4c48cfdccc95b3d68fb04d4be3995760517df7c7
Md. Arman Ali Ashik <armanaliashik115@gmail.com>
2026-03-30 03:18:30 +0600

    Update app.py

--- app.py
+++ app.py
@@ -124,5 +124,7 @@ def predict():
     except Exception as e:
         return jsonify({'error': f'Processing error: {str(e)}'}), 400
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render PORT
+    app.run(host="0.0.0.0", port=port)
